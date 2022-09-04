68f27248c7b3820b8ef306f92772bc3876f3e3b2
Naqi <inaqi69@gmail.com>
2022-09-04 16:22:24 +0500

    thumbnail preview added

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -229,11 +229,11 @@
                                 <div class="form-group">
                                     <h5>Main Thumbnail <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="file" name="product_thumbnail" class="form-control"> </div>
+                                        <input type="file" name="product_thumbnail" class="form-control" onchange="mainThumbUrl(this)"> </div>
                                         @error('product_thumbnail')
                                         <span class="text-danger">{{ $message }}</span>
-
                                         @enderror
+                                        <img src="" id="mainThumb" alt="">
                                 </div>
                             </div>
 
@@ -410,5 +410,16 @@
     });
 </script>
 
+<script>
+    function mainThumbUrl(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#mainThumb').attr('src',e.target.result).width(80).height(80);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
 
 @endsection
