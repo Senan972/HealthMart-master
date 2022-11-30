493c1d8739635fc162e6b3c7934d5f3b696d45d8
Naqi <inaqi69@gmail.com>
2022-11-30 11:38:23 +0500

    added more product details

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,6 +1,11 @@
 @extends('frontend.main_master')
 @section('content')
 
+
+@section('title')
+HealthMart
+@endsection
+
 <div class="body-content outer-top-xs" id="top-banner-and-menu">
   <div class="container">
     <div class="row"> 
